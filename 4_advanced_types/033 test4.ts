19ca8f3939047fc9459d205cb2c07fe4e0d4fc53
Денис Подсобилов <dpodsobilov29@gmail.com>
2023-07-25 11:10:28 +0400

    chapter 4 is complete

---
+++ 4_advanced_types/033 test4.ts
@@ -0,0 +1,51 @@
+interface IPayment {
+	sum: number;
+	from: number;
+	to: number;
+}
+
+enum PaymentStatus {
+	Success = 'success',
+	Failed = 'failed',
+}
+
+interface IPaymentRequest extends IPayment {}
+
+interface IDataSuccess extends IPayment {
+	databaseId: number;
+}
+
+interface IDataFailed {
+	errorMessage: string;
+	errorCode: number;
+}
+
+interface IResponseSuccess {
+	status: PaymentStatus.Success;
+	data: IDataSuccess;
+}
+
+interface IResponseFailed {
+	status: PaymentStatus.Failed;
+	data: IDataFailed;
+}
+
+type f = (res: IResponseSuccess | IResponseFailed) => number;
+
+type Res = IResponseSuccess | IResponseFailed;
+
+// type guard
+function isSuccess(res: Res): res is IResponseSuccess {
+	if (res.status === PaymentStatus.Success) {
+		return true;
+	}
+	return false;
+}
+
+const getIdFromData = (res: Res): number => {
+	if (isSuccess(res)) {
+		return res.data.databaseId;
+	} else {
+		throw new Error(res.data.errorMessage);
+	}
+};
