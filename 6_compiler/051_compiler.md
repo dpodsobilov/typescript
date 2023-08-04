b26afdd42ff84fde1d4ca860a5260c8194a9f993
Денис Подсобилов <dpodsobilov29@gmail.com>
2023-08-04 11:48:14 +0400

    lessons 51-53, configuring tsconfig

---
+++ 6_compiler/051_compiler.md
@@ -0,0 +1,14 @@
+Архитектура TypeScript
+
+Core TypeScript Compiler - основные библиотеки для компиляции TS в JS
+
+TSC - компилятор, вызываемый через терминал
+
+Language service - логика работы с языком
+ts.server
+
+             IDE
+
+          ts.server
+    Laguage Service     TSC
+    Core TypeScript Compiler
---
+++ 6_compiler/052_including_excluding_files.ts
@@ -0,0 +1,20 @@
+// tsc - compiles all files using tsconfig.json
+
+// in tsconfig.json
+//
+// ,
+// 	"files": [
+// 		"./6_compiler/052_including_excluding_files.ts"
+// 	]
+
+// or we can use include:
+//
+// "include": [
+//     "./6_compiler/05*"
+// ]
+
+// "exclude": [
+//     "./3_basic_types/*"
+// ]
+
+// extends - extends from other tsconfig
